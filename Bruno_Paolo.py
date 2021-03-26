dfbb720ea509ff81e485e0261e9dddf35da96040
paolobruno <71867663+paolobruno@users.noreply.github.com>
2021-03-26 17:32:07 +0100

    Add files via upload

---
+++ Bruno_Paolo.py
@@ -0,0 +1,42 @@
+import csv
+import pygames
+import sys
+
+alfa{"a":[0,0,0,0,1];
+           "b":[0,0,0,1,0],"c":[0,0,1,0,0],"d":[0,1,0,0,0],"e":[1,0,0,0,0],"f":[0,0,0,1,1];
+           "g":[0,0,1,1,0],"h":[0,1,1,0,0],"i":[1,1,0,0,0],"l":[0,0,1,1,1],"m":[0,1,1,1,0];
+           "n":[1,1,1,0,0],"o":[1,1,1,1,0],"p":[0,1,1,1,1],"q":[1,1,1,1,1],"r":[1,0,1,0,0],"s":[1,0,0,0,1];"t":[1,0,1,0,1],
+           "u":[1,0,0,1,0],"v":[1,1,0,1,1],"z":[0,1,1,0,1]}
+
+
+a = alfa["a"]
+b = alfa["b"]
+c = alfa["c"]
+d = alfa["d"]
+e = alfa["e"]
+f = alfa["f"]
+g = alfa["g"]
+h = alfa["h"]
+i = alfa["i"]
+l = alfa["l"]
+m = alfa["m"]
+n = alfa["n"]
+o = alfa["o"]
+p = alfa["p"]
+q = alfa["q"]
+r = alfa["r"]
+s = alfa["s"]
+t = alfa["t"]
+u = alfa["u"]
+v = alfa["v"]
+z = alfa["z"]
+
+lista = [list(a),list(b),list(c),list(d),list(e),list(f),list(g),list(h),list(i),
+        list(l),list(m),list(n),list(o),list(p)
+            list(q),list(r),list(s),list(t),list(u),list(v),list(z)]
+
+
+
+
+
+
